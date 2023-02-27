d88a4ecaf812e2b58ed5c4801690d4e25f738615
SmitasCodes <airidassmith30@gmail.com>
2023-02-27 19:41:34 +0200

    added middleware and some small variables

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-require("./config/databseConfig")()
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/databaseConfig");
+const port = process.env.PORT || 6655;
+
+connectDB();
+
 const app = express()
 
 app.use(cors())
@@ -9,4 +14,6 @@ app.options("*", cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(process.env.PORT, () => console.log("Listening on PORT", process.env.PORT))
+app.use(errorHandler);
+
+app.listen(port, () => console.log("Listening on PORT", port))
