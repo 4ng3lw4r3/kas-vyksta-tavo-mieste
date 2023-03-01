9c614539096dabfde7b13caa09d5879b31954f95
SmitasCodes <airidassmith30@gmail.com>
2023-03-01 15:46:44 +0200

    fixed routes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/databaseConfig");
 const port = process.env.PORT || 6655;
 
 connectDB();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.options("*", cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.options("*", cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use("/api/events", require("./routes/eventRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log("Listening on PORT", port))
-
-
+app.listen(port, () => console.log("Listening on PORT", port));
