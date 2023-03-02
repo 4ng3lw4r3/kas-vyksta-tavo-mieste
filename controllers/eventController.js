2c41c7d9cf6780a4fcb5a5518b11f961c524a030
SmitasCodes <airidassmith30@gmail.com>
2023-03-02 19:45:23 +0200

    fixed event update and delete controllers

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -25,90 +25,97 @@ const setEvent = asyncHandler(async (req, res) => {
     event_duration: req.body.event_duration,
   });
   res.status(200).json(createEvent);
-
 });
 
 // @desc get events
 // @route GET /api/events
 // @access Public
 const getEvents = asyncHandler(async (req, res) => {
-    const events = await eventSchema.find();
-    res.status(200).json(events);
-})
+  const events = await eventSchema.find();
+  res.status(200).json(events);
+});
 
 // @desc update events
 // @route PUT /api/events/:id
 // @access private
 const updateEvent = asyncHandler(async (req, res) => {
-    const event = await eventSchema.findById(req.params.id);
-    if (!event){
-        res.status(404);
-        throw new Error("Event not found");
-    }
+  const event = await eventSchema.findById(req.params.id);
 
-    // check for user
-    if (!req.user) { 
-        res.status(401);
-        throw new Error("User not found");
-    }
+  if (!event) {
+    res.status(404);
+    throw new Error("Event not found");
+  }
 
-    //make sure the logged in user matches the goal user
-    if (event.user.toString() !== req.user.toString()){
-        res.status(401);
-        throw new Error("Not authorized");
-    }
+  // check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
 
-    const updateEvent = await eventSchema.findByIdAndUpdate(req.params.id, req.body, {
+  //make sure the logged in user matches the goal user
+  if (event.host.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  const updateEvent = await eventSchema.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  //make sure the logged in user matches the goal user
+  if (event.host.toString() !== req.user.id && req.user.role) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  // if admin or event creator can edit
+  if (req.user.role === "admin" || event.host.toString() === req.user.id) {
+    const updateEvent = await eventSchema.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
         new: true,
-    })
+      }
+    );
+  }
 
-    //make sure the logged in user matches the goal user
-    if (event.user.toString() !== req.user.id && req.user.role){
-        res.status(401);
-        throw new Error("Not authorized");
-    }
-
-    // if admin or event creator can edit
-    if (req.user.role === "admin" || event.user.toString() === req.user.id){
-        const updateEvent = await eventSchema.findByIdAndUpdate(req.params.id, req.body ,{
-            new: true
-        })
-    }
-
-    res.status(200).json(updateEvent);
-})
+  res.status(200).json(updateEvent);
+});
 
 // @desc delete events
 // @route DELETE /api/events/:id
 // @access private
 
 const deleteEvent = asyncHandler(async (req, res) => {
-    const event = await eventSchema.findById(req.params.id);
-    if (!event){
-        res.status(404);
-        throw new Error("Event not found");
-    }
+  const event = await eventSchema.findById(req.params.id);
+  if (!event) {
+    res.status(404);
+    throw new Error("Event not found");
+  }
 
-    // check for user
-    if (!req.user) {
-        res.status(401);
-        throw new Error("User not found");
-    }
+  // check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
 
-    //make sure the logged in user matches the goal user
-    if (event.user.toString() !== req.user.toString()){
-        res.status(401);
-        throw new Error("Not authorized");
-    }
+  //make sure the logged in user matches the goal user
+  if (event.host.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
 
-    await event.remove();
-    res.status(200).json({message: "Event removed"});
-})
-
+  await event.remove();
+  res.status(200).json({ message: "Event removed" });
+});
 
 module.exports = {
-    setEvent,
-    getEvents,
-    updateEvent,
-    deleteEvent
+  setEvent,
+  getEvents,
+  updateEvent,
+  deleteEvent,
 };
--- models/EventModel.js
+++ models/EventModel.js
@@ -1,32 +1,33 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const eventSchema = mongoose.Schema(
-    {
-        host: {
-            type: String,
-            required: [true, "Please add the host"],
-        },
-        email: {
-            type: String,
-            required: [true, "Please add your contact email"],
-            unique: true,
-        },
-        event_location: {
-            type: String,
-            required: [true, "Please add the event's location"],
-        },
-        event_date: {
-            type: String,
-            required: [true, "Please add the event's date"],
-        },
-        event_duration: {
-            type: String,
-            required: [true, "Please add the event's duration"],
-        },
+  {
+    host: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
-    {
-        timestamps: true,
-    }
-)
+    email: {
+      type: String,
+      required: [true, "Please add your contact email"],
+      unique: true,
+    },
+    event_location: {
+      type: String,
+      required: [true, "Please add the event's location"],
+    },
+    event_date: {
+      type: String,
+      required: [true, "Please add the event's date"],
+    },
+    event_duration: {
+      type: String,
+      required: [true, "Please add the event's duration"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("Event", eventSchema)
+module.exports = mongoose.model("Event", eventSchema);
